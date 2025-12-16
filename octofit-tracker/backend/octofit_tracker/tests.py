8fcaef4c359be9980ed1fa664b6cd40debff9d5c
Keeiith99 <ivy15.kis@gmail.com>
2025-12-16 06:48:49 +0000

    commit update octofit-tracker app to use MongoDB and add core features

---
+++ octofit-tracker/backend/octofit_tracker/tests.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from .models import User, Team, Activity, Workout, Leaderboard
+
+class ModelSmokeTest(TestCase):
+    def test_team_create(self):
+        team = Team.objects.create(name='Marvel')
+        self.assertEqual(str(team), 'Marvel')
+    def test_user_create(self):
+        team = Team.objects.create(name='DC')
+        user = User.objects.create(name='Clark Kent', email='clark@dc.com', team=team)
+        self.assertEqual(str(user), 'Clark Kent')
